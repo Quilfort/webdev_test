ca387e9afacaceb2250eca563b255cc8376bd890
Quilfort <qfrederi@gmail.com>
2023-11-29 09:50:01 +0100

    Restructur and added Lauravel

---
+++ Lauravel_PHP/test_projects/example-app/app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = BlogPost::all(); //fetch all blog posts from DB
+	    return $posts; //returns the fetched posts
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BlogPost $blogPost)
+    {
+        return $blogPost; //returns the fetched posts
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BlogPost $blogPost)
+    {
+        //
+    }
+}
