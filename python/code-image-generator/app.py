ae2cbd38a09a8a50b57b5a2ee7ca257b9dde3533
Quilfort <qfrederiks@gmail.com>
2023-12-13 13:25:37 +0100

    Form to add code, Form changes with len lines

--- python/code-image-generator/app.py
+++ python/code-image-generator/app.py
@@ -1,15 +1,40 @@
 # app.py
+# Secret is fake
 
-from flask import Flask, render_template
+from flask import (
+    Flask, 
+    render_template, 
+    session,
+    redirect,
+    request,
+    url_for,
+)
 
 app = Flask(__name__)
+app.secret_key = "f78c8c63eacaf7570f440166bd30c43ad6ad0808a76770f08ae9038f0a81ced8"
 
 PLACEHOLDER_CODE = "print('Hello, World!')"
 
 @app.route("/", methods=["GET"])
 def code():
+    if session.get("code") is None: 
+        session["code"] = PLACEHOLDER_CODE
+    lines = session["code"].split("\n")
     context = {
         "message":  "Check your Python Code",
-        "code": PLACEHOLDER_CODE,
+        "code": session["code"],
+        "num_lines": len(lines),
+        "max_chars": len(max(lines, key=len))
     }
     return render_template("code_input.html", **context)
+
+@app.route("/save_code", methods=["POST"])
+def save_code():
+    session["code"] = request.form.get("code")
+    return redirect(url_for("code"))
+
+@app.route("/reset_session", methods=["POST"])
+def reset_session():
+    session.clear()
+    session["code"] = PLACEHOLDER_CODE
+    return redirect(url_for("code"))
