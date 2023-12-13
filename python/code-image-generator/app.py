c26f0e98c5c6f5d1ef1c5e918f2ca172c799f6b1
Quilfort <qfrederiks@gmail.com>
2023-12-13 15:21:49 +0100

    Image not working yet

--- python/code-image-generator/app.py
+++ python/code-image-generator/app.py
@@ -10,10 +10,20 @@ from flask import (
     url_for,
 )
 
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import Python3Lexer
+from pygments.styles import get_all_styles
+
+import base64
+
+# from my_utils import take_screenshot_from_url
+
 app = Flask(__name__)
 app.secret_key = "f78c8c63eacaf7570f440166bd30c43ad6ad0808a76770f08ae9038f0a81ced8"
 
 PLACEHOLDER_CODE = "print('Hello, World!')"
+DEFAULT_STYLE = "monokai"
 
 @app.route("/", methods=["GET"])
 def code():
@@ -38,3 +48,43 @@ def reset_session():
     session.clear()
     session["code"] = PLACEHOLDER_CODE
     return redirect(url_for("code"))
+
+@app.route("/save_style", methods=["POST"])
+def save_style():
+    if request.form.get("style") is not None:
+        session["style"] = request.form.get("style")
+    if request.form.get("code") is not None:
+        session["code"] = request.form.get("code")
+    return redirect(url_for("style"))
+
+@app.route("/style", methods=["GET"])
+def style():
+    if session.get("style") is None:
+        session["style"] = DEFAULT_STYLE
+    formatter = HtmlFormatter(style=session["style"])
+    context = {
+        "message": "Select Your Style by Q",
+        "all_styles": list(get_all_styles()),
+        "style": session["style"],
+        "style_definitions": formatter.get_style_defs(),
+        "style_bg_color": formatter.style.background_color,
+        "highlighted_code": highlight(
+            session["code"], Python3Lexer(), formatter
+        ),
+    }
+    return render_template("style_selection.html", **context)
+
+# @app.route("/image", methods=["GET"])
+# def image():
+#     session_data = {
+#         "name": app.config["SESSION_COOKIE_NAME"],
+#         "value": request.cookies.get(app.config["SESSION_COOKIE_NAME"]),
+#         "url": request.host_url,
+#     }
+#     target_url = request.host_url + url_for("style")
+#     image_bytes = take_screenshot_from_url(target_url, session_data)
+#     context = {
+#         "message": "Done! 🎉",
+#         "image_b64": base64.b64encode(image_bytes).decode("utf-8"),
+#     }
+#     return render_template("image.html", **context)
